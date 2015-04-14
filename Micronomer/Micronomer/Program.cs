b4379ec1aadeeb6302d01d5c46314bb815829c70
katascope <something@somewhere>
2015-04-13 18:01:44 -0700

    Initial Micronomer load
    Basic hello world initial test

---
+++ Micronomer/Micronomer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Micronomer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Micronomer");
+        }
+    }
+}
